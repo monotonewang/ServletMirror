42746a6b1beff2c651f8aa4f605261646b33fa1f
montotone <zgjxfuqingwang@gmail.com>
2016-11-26 18:42:39 +0800

    add -->servletdemo

---
+++ src/com/demo/servletpath/servlet/ServletDemo4.java
@@ -0,0 +1,29 @@
+package com.demo.servletpath.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 分析service的方法	doXXX
+ * @author Administrator
+ *
+ */
+public class ServletDemo4 extends HttpServlet{
+
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+	}
+	
+	public void service(ServletRequest req, ServletResponse res)
+			throws ServletException, IOException {
+		
+	}
+	
+}
