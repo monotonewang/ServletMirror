da706b8ce421d4f6414969f810fc9b1811e40e5a
montotone <zgjxfuqingwang@gmail.com>
2016-12-01 20:48:14 +0800

    add -->mysql.txt

--- src/com/demo/servletpath/response/ServletMyInterFace.java
+++ src/com/demo/servletpath/response/ServletMyInterFace.java
@@ -15,20 +15,22 @@ import java.util.List;
 /**
  * Created by 7 on 2016/11/28.
  */
-@WebServlet(name = "ServletMyFirstInterFace")
-public class ServletMyFirstInterFace extends HttpServlet {
+@WebServlet(name = "ServletMyInterFace")
+public class ServletMyInterFace extends HttpServlet {
     Dao dao = new Dao();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String num = request.getParameter("num");
+        int index=Integer.parseInt(num);
         String sql = "select FlowID flowID,Type type,IdCard idCard,ExamStudent examStudent,StudentName studentName,Location location,Gradle gradle from examstudent";
         List<Student> student = dao.getForList(Student.class, sql);
         //截取
-        List<Student> students = student.subList(0, 3);
+        List<Student> students = student.subList(0, index );
         String str = JSONObject.toJSONString(students);
         response.getWriter().write(str);
     }
