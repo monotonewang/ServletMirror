64d5d2e02bf015d77fdbb5a88623ef24df682583
montotone <zgjxfuqingwang@gmail.com>
2016-12-05 02:49:32 +0800

    add -->sessioncart

--- src/com/demo/servletpath/session/ServletSession.java
+++ src/com/demo/servletpath/session/ServletSession.java
@@ -5,7 +5,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by WangTest on 2016/12/3.
@@ -17,6 +19,41 @@ public class ServletSession extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //判断是否是第一次登陆，
+        String id = request.getParameter("id");
+        String names[] ={"手电筒","冰箱","电脑","电视机","电话"};
+        if(id==null){
+            return;
+        }
+        int idx=Integer.parseInt(id);
+        //获取商品名称
+        String name=names[idx-1];
+        //从session中获取购物车
+        HttpSession session = request.getSession();
+        //获取购物车
+        HashMap<String,Integer> cartMap = (HashMap) session.getAttribute("cart");
+           if(cartMap==null){
+               //创建购物车
+               cartMap=new HashMap<>();
+               //第一次访问
+               cartMap.put(name,1);
+               session.setAttribute("cart",cartMap);
+           }else{
+               //不是第一次访问,是否包含该值
+                if(cartMap.containsKey(name)){
+                    //取出数量 +1.存入购物车
+                    Integer integer = cartMap.get(name);
+                    integer++;
+                    cartMap.put(name,integer);
+                    session.setAttribute(name,cartMap);
+                }else{
+                    //不包含
+                    cartMap.put(name,1);
+                    session.setAttribute("cart",cartMap);
+                }
+           }
+           response.setContentType("text/html;charset=UTF-8");
+           response.getWriter().write("<h3><a href='/ServletMirror/jsp/cartSession.jsp'>继续购物</a><a href='/ServletMirror/jsp/paySession.jsp'>结算</a></h3>");
 
     }
 }
---
+++ web/jsp/cartSession.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: WangTest
+  Date: 2016/12/5
+  Time: 2:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>shopping Cart</title>
+</head>
+<body>
+    <h3>手电筒<a href="/ServletMirror/ServletSession?id=1">加入购物车</a></h3>
+    <h3>冰箱<a href="/ServletMirror/ServletSession?id=2">加入购物车</a></h3>
+    <h3>电脑<a href="/ServletMirror/ServletSession?id=3">加入购物车</a></h3>
+    <h3>电视机<a href="/ServletMirror/ServletSession?id=4">加入购物车</a></h3>
+    <h3>电话<a href="/ServletMirror/ServletSession?id=5">加入购物车</a></h3>
+    <h3>继续购物车</h3>
+    <h3>结算</h3>
+</body>
+</html>
