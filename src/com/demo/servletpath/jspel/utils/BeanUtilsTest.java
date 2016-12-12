c20478d96db4d67e1bb0f4aa617827cad7bd848a
montotone <zgjxfuqingwang@gmail.com>
2016-12-12 13:34:24 +0800

    add -->Introspector

--- src/com/demo/servletpath/jspel/utils/BeanUtilsTest.java
+++ src/com/demo/servletpath/jspel/utils/BeanUtilsTest.java
@@ -1,29 +1,56 @@
 package com.demo.servletpath.jspel.utils;
 
-import com.demo.servletpath.jdbc.bean.User;
 import org.apache.commons.beanutils.BeanUtils;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 
 
-
 public class BeanUtilsTest {
 	@Test
-	public void testGetUser() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException{
-		Object object=new User();
-		
-		BeanUtils.setProperty(object, "name", "21212");
+	public void testGetUser() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		Object object = new Student();
+		BeanUtils.setProperty(object, "setName", "test");
 		System.out.println(object);
-		Object val=BeanUtils.getProperty(object, "name");
+		Object val = BeanUtils.getProperty(object, "name");
 		System.out.println(val);
 	}
+
 	@Test
 	public void test() throws IllegalAccessException, InvocationTargetException {
-		Object object=new User();
-
-		BeanUtils.setProperty(object, "name", "21212");
-		System.out.println(object);
+		Student student = new Student();
+		String sname = "abv";
+		BeanUtils.setProperty(student, "name", sname);
+		System.out.println(student.getName());
 	}
 
+}
+
+class Student {
+	String name;
+	String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{" +
+				"name='" + name + '\'' +
+				", password='" + password + '\'' +
+				'}';
+	}
 }
--- src/com/demo/servletpath/jspel/utils/IntrospectorTest.java
+++ src/com/demo/servletpath/jspel/utils/IntrospectorTest.java
@@ -1,6 +1,5 @@
 package com.demo.servletpath.jspel.utils;
 
-import com.demo.servletpath.jspel.bean.User;
 import org.junit.Test;
 
 import java.beans.BeanInfo;
@@ -14,22 +13,46 @@ import java.lang.reflect.Method;
 public class IntrospectorTest {
     @Test
     public void run() throws Exception{
+        String proName="class";
         User user = new User();
         // 获取类的信息
         BeanInfo info = Introspector.getBeanInfo(user.getClass());
         // 获取属性的描述
         PropertyDescriptor[] pds = info.getPropertyDescriptors();
+
         // 循环遍历，获取属性的名称
         for (PropertyDescriptor pd : pds) {
-            // System.out.println(pd.getName());
-            if(!"class".equals(pd.getName())){
+//             System.out.println(pd.getName());
+            if(!proName.equals(pd.getName())){
                 // 获取写的方法
                 Method m = pd.getWriteMethod();
                 m.invoke(user, "admin");
             }
         }
 
-        System.out.println(user.getUsername());
+        System.out.println(user.getName());
         System.out.println(user.getPassword());
     }
+}
+
+class User {
+    String name;
+    String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
