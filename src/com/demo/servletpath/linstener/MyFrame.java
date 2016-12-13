49ef20bac2b1a388f3fa4821304c447b6f0c3725
montotone <zgjxfuqingwang@gmail.com>
2016-12-13 14:07:37 +0800

    add -->listener file

---
+++ src/com/demo/servletpath/linstener/MyFrame.java
@@ -0,0 +1,37 @@
+package com.demo.servletpath.linstener;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame {
+
+	private JButton but = new JButton("按钮"); // 事件源
+
+	// 事件:but被按下就是一个事件。 ActionEvent 对于ActionEvent事件的监听器就是ActionListener.
+
+	private MyFrame() {
+		this.getContentPane().add(but);
+
+		but.addActionListener(new ButListener()); // 注册监听
+
+		this.setDefaultCloseOperation(3);
+		this.setBounds(100, 100, 300, 300);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new MyFrame();
+	}
+}
+
+// 监听器
+class ButListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+
+		// e.getSource() 获取事件源
+
+		System.out.println("hello world");
+	}
+}
