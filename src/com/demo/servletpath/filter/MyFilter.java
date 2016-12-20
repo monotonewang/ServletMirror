6fee823fa0239e9d5a4f0e2916ab068fce1ecd3e
montotone <zgjxfuqingwang@gmail.com>
2016-12-20 15:33:30 +0800

    add -->file upload

--- src/com/demo/servletpath/filter/MyFilter.java
+++ src/com/demo/servletpath/filter/MyFilter.java
@@ -15,31 +15,49 @@ public class MyFilter implements Filter {
 	int count = 200;
 	private ServletContext servletContext;
 
-	public void destroy() {
-	}
-
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-		System.out.println("firstFilter.......");
-		chain.doFilter(req, resp);//向下放行
-		System.out.println("firstFilter end.....");
-		count++;
-		HttpServletResponse response = (HttpServletResponse) resp;
-//		response.getWriter().write("count=" + count);
-	}
-
+	/**
+	 * when start server call this method
+	 * @param config
+	 * @throws ServletException
+	 */
 	public void init(FilterConfig config) throws ServletException {
 		//获取Filter 名称
 		String filterName = config.getFilterName();
 //		System.out.println("filterName"+filterName);
+
 		//获取初始化参数
 		String encoding = config.getInitParameter("encoding");
 //		System.out.println("encoding"+encoding);
 		String username = config.getInitParameter("username");
 //		System.out.println("username"+username);
+
 		//get servletContext
 		//获取全局对象
 		servletContext = config.getServletContext();
+	}
 
+	/**
+	 *  when call filter-mapping page call this method
+	 * @param req
+	 * @param resp
+	 * @param chain
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+//		System.out.println("firstFilter.......");
+		chain.doFilter(req, resp);//向下放行
+//		System.out.println("firstFilter end.....");
+		count++;
+		HttpServletResponse response = (HttpServletResponse) resp;
+//		response.getWriter().write("count=" + count);
+	}
+
+	/**
+	 * when stop server call this method
+	 */
+	public void destroy() {
+//		System.out.println("MyFilter-->destroy");
 	}
 
 }
