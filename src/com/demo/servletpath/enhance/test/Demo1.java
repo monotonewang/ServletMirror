9786776a7aacda824357a33c09fcf9caa41cb617
montotone <zgjxfuqingwang@gmail.com>
2016-12-27 16:39:11 +0800

    add-->gson

--- src/com/demo/servletpath/enhance/test/Demo1.java
+++ src/com/demo/servletpath/enhance/test/Demo1.java
@@ -1,21 +1,19 @@
 package com.demo.servletpath.enhance.test;
 
 public class Demo1 {
-	
+
 	public static void main(String[] args) {
 		int sum = 0;
 		// 从1加到100的结果
-		for(int i=1;i<=100;i++){
+		for (int i = 1; i <= 100; i++) {
 			sum += i;
 		}
 		System.out.println(sum);
 	}
-	
-	
-	public static void runTest(){
+
+
+	public static void runTest() {
 		System.out.println("hello java");
 	}
-	
-	
 
 }
